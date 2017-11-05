8f30d0c669315053c19645af2fb25154659b96dc
Alexander Pearson-Goulart <pearsongoulart@gmail.com>
2017-11-05 09:48:58 -0800

    Stashing

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -22,6 +22,12 @@ All Events
         <div class="col-9 pl-5 pr-5 pt-4">
             <?php
 
+
+                // Are there events? Used for styling page
+                $future = false;
+                $present = false;
+                $past = false;
+
                 $userId = Auth::user()->id;
 
                 //Select all this user's future events
@@ -30,22 +36,33 @@ All Events
                                     ->orderBy('starts', 'DESC')
                                     ->get();
 
+                if($futureEvents != null) {
+                    $future = true;
+                }
+
                 //Select all this user's present events
                 $presentEvents = App\Event::where('user_id', $userId)
                                     ->where('time_state','=', 1)
                                     ->orderBy('starts', 'DESC')
                                     ->get();
 
+                if($presentEvents != null) {
+                    $present = true;
+                }
+
                 //Select all this user's past events
                 $pastEvents = App\Event::where('user_id', $userId)
                                     ->where('time_state','=', 0)
                                     ->orderBy('starts', 'DESC')
                                     ->get();
+
+                if($pastEvents != null) {
+                    $past = true;
+                }
             ?>
 
             @if(isset($futureEvents[0]))
-                <?php echo '<br /><hr /><br />
-                <h3 class="text center">Future Events</h3>'?>
+                <?php echo '<br /><h2 class="text center bold">Future Events</h2>'?>
                 @foreach($futureEvents as $event)
                     @if($event->user->id == Auth::user()->id)
                         @include('events.event')
@@ -54,8 +71,15 @@ All Events
             @endif
 
             @if(isset($presentEvents[0]))
-                <?php echo '<br /><hr /><br />
-                <h3 class="text center">Current Events</h3>'?>
+                <?php 
+                if(!$future) {
+                    echo '<h2 class="text center bold">Current Events</h2>';
+                }
+                else {
+                    echo '<br /><h2 class="text center bold">Current Events</h2>
+                    <hr />';
+                }
+                ?>
                 @foreach($presentEvents as $event)
                     @if($event->user->id == Auth::user()->id)
                         @include('events.event')
@@ -64,8 +88,15 @@ All Events
             @endif
 
             @if(isset($pastEvents[0]))
-                <?php echo '<br /><hr /><br />
-                <h3 class="text center">Past Events</h3>'?>
+                <?php
+                if(!$future && !$present) {
+                    echo '<h2 class="text center bold">Past Events</h2>';
+                }
+                else {
+                    echo '<br /><h2 class="text center bold">Past Events</h2>
+                    <hr />';
+                }
+                ?>
                 @foreach($pastEvents as $event)
                     @if($event->user->id == Auth::user()->id)
                         @include('events.event')
@@ -82,5 +113,13 @@ All Events
 <style>
     footer {
         position: relative !important;
+    }
+
+    .bold {
+        font-weight: 300 !important;
+    }
+
+    hr {
+        margin-bottom: 2.5rem !important;
     }
 </style>
